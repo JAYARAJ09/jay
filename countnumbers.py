a7ba22795706069f31c29cc4fa0784722b1dd14c
JAYARAJ09 <46372079+JAYARAJ09@users.noreply.github.com>
2019-02-03 20:52:10 -0800

    Update countnumbers.py

--- countnumbers.py
+++ countnumbers.py
@@ -1,6 +1,6 @@
-jay=int(input("Enter number:"))
+jay=int(input())
 count=0
 while(jay>0):
-	count=count+1
-	jay=jay//10
-	print("the number of digits in the integer are:",count)
+    count=count+1
+    jay=jay//10
+print(count)
