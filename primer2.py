91a0c68e065bb9bae4315a59e9abf7199e65487a
JAYARAJ09 <46372079+JAYARAJ09@users.noreply.github.com>
2019-02-25 16:10:57 +0530

    Update primer2.py

--- primer2.py
+++ primer2.py
@@ -1,6 +1,6 @@
-t =raw_input()
-t=t.split()
-for num in range(int(t[0])+1,int(t[1])):  
+b =raw_input()
+b=b.split()
+for num in range(int(b[0])+1,int(b[1])):  
    if num > 1:  
        for i in range(2,num):  
            if (num % i) == 0:  
